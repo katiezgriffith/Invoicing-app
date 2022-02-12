5db224755e9cd00862de30f463032bffe2fbfbca
Katie Griffith <katiezgriffith@gmail.com>
2022-02-12 07:06:21 -0600

    Implemented migraton routes and testing creating tables

--- database/migrations/1.0.js
+++ database/migrations/1.0.js
@@ -15,3 +15,37 @@ module.exports = {
   
         // ...
   
+       // ...
+
+      db.serialize(function() {
+        db.run(`CREATE TABLE users (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          email TEXT,
+          company_name TEXT,
+          password TEXT
+        )`);
+
+        db.run(`CREATE TABLE invoices (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          user_id INTEGER,
+          paid NUMERIC,
+          FOREIGN KEY(user_id) REFERENCES users(id)
+        )`);
+
+        db.run(`CREATE TABLE transactions (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          price INTEGER,
+          invoice_id INTEGER,
+          FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+        )`);
+      });
+
+      db.close();
+    });
+  }
+
+}
+ 
--- server.js
+++ server.js
@@ -24,3 +24,70 @@ app.listen(PORT, function() {
     console.log(`App running on localhost:${PORT}.`);
   });
   
+
+  // ...
+
+const _ = require('lodash');
+
+const multer  = require('multer');
+const upload = multer();
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// POST /register - begin
+
+app.post('/register', upload.none(), function(req, res) {
+  // check to make sure none of the fields are empty
+  if (
+    _.isEmpty(req.body.name)
+    || _.isEmpty(req.body.email)
+    || _.isEmpty(req.body.company_name)
+    || _.isEmpty(req.body.password)
+  ) {
+    return res.json({
+      "status": false,
+      "message": "All fields are required."
+    });
+  }
+
+  // any other intended checks
+
+// ...
+
+bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+    let db = new sqlite3.Database('./database/InvoicingApp.db');
+
+    let sql = `INSERT INTO
+                users(
+                  name,
+                  email,
+                  company_name,
+                  password
+                )
+                VALUES(
+                  '${req.body.name}',
+                  '${req.body.email}',
+                  '${req.body.company_name}',
+                  '${hash}'
+                )`;
+
+    db.run(sql, function(err) {
+      if (err) {
+        throw err;
+      } else {
+        return res.json({
+          "status": true,
+          "message": "User Created."
+        });
+      }
+    });
+
+    db.close();
+  });
+
+});
+
+// POST /register - end
+
